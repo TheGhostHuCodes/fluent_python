dbf1323eab9f4f5c95b19dab13ed55de1fd8a21b
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-08-23 23:35:23 -0700

    Add functions for shaving combining marks from base characters

---
+++ chapter_04_text_versus_bytes/sanitize.py
@@ -0,0 +1,25 @@
+import unicodedata
+import string
+
+
+def shave_marks(txt):
+    """Remove all diacritic marks"""
+    norm_txt = unicodedata.normalize('NFD', txt)
+    shaved = ''.join(c for c in norm_txt if not unicodedata.combining(c))
+    return unicodedata.normalize('NFC', shaved)
+
+
+def shave_marks_latin(txt):
+    """Remove all diacritic marks from Latin base characters"""
+    norm_txt = unicodedata.normalize('NFD', txt)
+    latin_base = False
+    keepers = []
+    for c in norm_txt:
+        if unicodedata.combining(c) and latin_base:
+            continue  # Ignore diacritic on latin base char.
+        keepers.append(c)
+        # If it isn't combining char, it's a new base char.
+        if not unicodedata.combining(c):
+            latin_base = c in string.ascii_letters
+    shaved = ''.join(keepers)
+    return unicodedata.normalize('NFC', shaved)
