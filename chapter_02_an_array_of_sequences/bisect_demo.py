faf1fd319676b21b607bcf961d8b25e28c715ac5
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-02-19 08:14:01 -0800

    Refactor bisect demo

--- chapter_02_an_array_of_sequences/bisect_demo.py
+++ chapter_02_an_array_of_sequences/bisect_demo.py
@@ -7,9 +7,9 @@ NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
 ROW_FMT = '{0:2d} @ {1:2d}    {2}{0:<2d}'
 
 
-def demo(bisect_fn):
+def demo(bis_fn):
     for needle in reversed(NEEDLES):
-        position = bisect_fn(HAYSTACK, needle)
+        position = bis_fn(HAYSTACK, needle)
         offset = position * '  |'
         print(ROW_FMT.format(needle, position, offset))
 
