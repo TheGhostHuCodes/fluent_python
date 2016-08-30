8249def620adf2cd9a02bade77e9719187ceeba4
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-08-29 21:30:26 -0700

    Add methods to sanitize unicode strings to ASCII

--- chapter_04_text_versus_bytes/sanitize.py
+++ chapter_04_text_versus_bytes/sanitize.py
@@ -23,3 +23,29 @@ def shave_marks_latin(txt):
             latin_base = c in string.ascii_letters
     shaved = ''.join(keepers)
     return unicodedata.normalize('NFC', shaved)
+
+
+single_map = str.maketrans("""‚ƒ„†ˆ‹‘’“”•–—˜›""", """'f"*^<''""---~>""")
+
+multi_map = str.maketrans({
+    '€': '<euro>',
+    '…': '...',
+    'Œ': 'OE',
+    '™': '(TM)',
+    'œ': 'oe',
+    '‰': '<per mille>',
+    '‡': '**',
+})
+
+multi_map.update(single_map)
+
+
+def dewinize(txt):
+    """Replace Win1252 symbols with ASCII chars or sequences"""
+    return txt.translate(multi_map)
+
+
+def asciize(txt):
+    no_marks = shave_marks_latin(dewinize(txt))
+    no_marks = no_marks.replace('ß', 'ss')
+    return unicodedata.normalize('NFKC', no_marks)
